f8a32fe3e2f0d7a81c61d2dc85bd712c09b66556
towanglei <towanglei@163.com>
2016-06-21 11:24:37 +0800

    单元测试项目

---
+++ Demo/Test/BThread_test.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h" 
+//#include "BThread.hpp"
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	BThread bThread;
+//	bool rtVal = bThread.Create(100, 100, true);
+//	//rtVal = bThread.Resume();
+//	//this_thread::sleep(boost::get_system_time() + posix_time::milliseconds(3000));
+//	rtVal = bThread.Suspend();
+//	this_thread::sleep(boost::get_system_time() + posix_time::milliseconds(10));
+//	//rtVal = bThread.Click();
+//	//this_thread::sleep(boost::get_system_time() + posix_time::milliseconds(200));
+//	//rtVal = bThread.Click();
+//	//this_thread::sleep(boost::get_system_time() + posix_time::milliseconds(200));
+//	//rtVal = bThread.Click();
+//	//this_thread::sleep(boost::get_system_time() + posix_time::milliseconds(200));
+//	//rtVal = bThread.Click();
+//	//this_thread::sleep(boost::get_system_time() + posix_time::milliseconds(200));
+//	//rtVal = bThread.Click();
+//	//this_thread::sleep(boost::get_system_time() + posix_time::milliseconds(200));
+//	//rtVal = bThread.Click();
+//	//this_thread::sleep(boost::get_system_time() + posix_time::milliseconds(200));
+//	//rtVal = bThread.Resume();
+//	//this_thread::sleep(boost::get_system_time() + posix_time::milliseconds(200));
+//	rtVal = bThread.ForceEnd();
+//	this_thread::sleep(boost::get_system_time() + posix_time::seconds(30));
+//}
---
+++ Demo/Test/Container_test.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h" 
+//#include "Container.hpp"
+//ObjectArray<int,11> objectArray;
+//PointerPool_base pointerPool_base;
+//PointerPool_templated<3> pointerPool_templated;
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	PointerPool_singleton::GetInstance()->Add((void*)1);
+//	PointerPool_singleton::GetInstance()->Add((void*)2);
+//	PointerPool_singleton::GetInstance()->Add((void*)3);
+//	PointerPool_singleton::GetInstance()->Add((void*)4);;
+//	PointerPool_singleton::GetInstance()->Del((void*)2);
+//	PointerPool_singleton::GetInstance()->Add((void*)3);
+//	PointerPool_singleton::GetInstance()->Del((void*)4);
+//
+//	pointerPool_templated.Add((void*)1);
+//	pointerPool_templated.Add((void*)2);
+//	pointerPool_templated.Add((void*)3);
+//	pointerPool_templated.Add((void*)4);;
+//	pointerPool_templated.Del((void*)2);
+//	pointerPool_templated.Add((void*)3);
+//	pointerPool_templated.Del((void*)4);
+//}
